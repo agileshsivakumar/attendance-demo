641dab4337d183c9cec7d48f547d4b1cfcf10b0b
Agilesh Sivakumar <agilesh.sivakumar@aciworldwide.com>
2019-07-18 20:04:48 +0530

    project done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'attendance-demo';
+  public attendees: string[] = [
+    'Agilesh Sivakumar',
+    'Aaquib Nawaz',
+    'Chandrasekaran T D',
+    'Mahalakshmi Jayaprathap',
+    'Ramakanth Chintalapalli',
+    'Ashish Mallegowda',
+    'Bhagyashri Dhoop',
+    'Manpreet Singh',
+    'Hardik Sethi',
+    'Paramesh Gujjaiah',
+    'Gopalakrishna Chandregowda',
+    'Priya Khirwal',
+    'Sarath Sasidharan',
+    'Sumesh Chemmangattil',
+    'Raunak Sinha',
+    'Rohit Kumar',
+    'Pranav Gauba',
+    'Anjana Patra',
+    'Dinesh Malviya',
+    'Mohit Raj',
+    'Ooha Bhoga',
+    'Shilpa Reddy'
+  ];
 }
